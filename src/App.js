c4fcee7a0acb1952eb7e0cfbf73d76d12c2ee726
Ellie <camera20120109@gmail.com>
2024-01-31 09:45:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import userIcon from './user.svg';
+import warningIcon from './warning.svg';
+import commentIcon from './comment.svg';
+import printIcon from './print.svg';
+import ellie from './ellie.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{display:'flex',
+                   flexDirection:'column',
+                   justifyContent:'center',alignItems:'center'}}>
+      <div style= {{width:'992px',
+                    height:'28px',
+                    display:'flex',
+                    justifyContent:'flex-end',
+                    // marginBottom: '16px'
+                    }}>
+                      <button className='iconButton'><img src= {userIcon} alt="user" /></button>
+                      <button className='iconButton'><img src= {commentIcon} alt="user" /></button>
+                      <button className='iconButton'><img src= {printIcon} alt="user" /></button>
+                      <button className='iconButton'><img src= {warningIcon} alt="user" /></button>                                                                    
+      </div>
+      
+
+      <div style={{
+        backgroundColor:'#378AAD',
+        width:'992px',
+        height:'647px',
+        padding:'50px,10px,25px,10px'       
+      }}>
+          <div style={{
+            width:'119px',
+            height:'119px',
+            borderRadius:'50%',
+            overflow:'hidden',
+            margin:'50px'
+          }}>
+            <img src ={ellie} alt= "ellie" style={{width:'100%',
+                                      height:'100%'}}></img>
+          </div>
+        </div>
+
+
+      
     </div>
   );
 }
